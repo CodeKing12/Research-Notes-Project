7ccae6f787f8e496d1aa16274e2d07ec9ab3872f
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-07-14 05:19:47 +0100

    Changed staticfiles storage.

--- research_notes/views.py
+++ research_notes/views.py
@@ -31,6 +31,7 @@ def sortPopularity(item):
 def sortTagName(item):
     return item.name
 
+
 @login_required
 def homepage(request):
     all_tags = Tags.objects.all()
@@ -40,6 +41,7 @@ def homepage(request):
     nav_html = root_parent.get_folder_tree()
     all_types = Type.objects.all()
     return render(request, 'home.html', {"nav_html": nav_html, "all_tags": all_tags, "all_types": all_types})
+
 
 @login_required
 def resolveObject(request, path_to_file):
@@ -72,10 +74,12 @@ def resolveObject(request, path_to_file):
 
     return response
 
+
 @login_required        
 def displayFile(request, file):
     # print(file.papers.bibtex.file)
     return render(request, "file-display.html", {"file": file})
+
 
 @login_required
 def displayFolder(request, folder, sort="title", group="none"):
@@ -134,6 +138,7 @@ def displayFolder(request, folder, sort="title", group="none"):
     group_options = ["None", "Status", "Tags"]
     return render(request, "folder-tree.html", {"folder_name": folder_name, "folder_tree":  generated_html, "folder": folder, "group_list": group_list, "current_sorting": current_sorting, "current_grouping": current_grouping, "sort_options": sort_options, "group_options": group_options})
 
+
 @login_required
 def all_tags(request, sort="popularity", ascending=False):
     # If the filter form is submitted, update the sorting values.
@@ -165,6 +170,7 @@ def all_tags(request, sort="popularity", ascending=False):
     order_options = ["Ascending", "Descending"]
     # print(all_tags.notes)
     return render(request, "all_tags.html", {"all_tags": all_tags, "current_sorting": current_sorting, "current_ordering": current_ordering, "sort_options": sort_options, "order_options": order_options})
+
 
 @login_required
 def all_statuses(request, sort="title", ascending=False):
@@ -203,15 +209,18 @@ def all_statuses(request, sort="title", ascending=False):
         current_ordering = "Descending"
     return render(request, "all_statuses.html", {"group_list": group_list, "all_notes": all_notes ,"sort_options": sort_options, "order_options": order_options, "current_sorting": current_sorting , "current_ordering": current_ordering})
 
+
 @login_required
 def tag(request, tag_name):
     tag = Tags.objects.get(name=tag_name)
     return render(request, "tag.html", {"tag": tag})
 
+
 @login_required
 def status(request, status_name):
     status_list = normalNotes.objects.filter(status=status_name)
     return render(request, "status.html", {"status": status_name, "status_list": status_list})
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -230,6 +239,7 @@ def login_view(request):
         else:
             print("Error")
     return render(request, "login.html")
+
 
 @login_required
 def search_results(request, sort="name", group="none", ascending=True):
@@ -301,6 +311,7 @@ def search_results(request, sort="name", group="none", ascending=True):
         search = ""
     return render(request, "search-results.html", {"folder_results": folder_results, "file_results": file_results, "folders": all_folders, "all_types": all_types, "search_term": search, "sort_options": sort_options, "group_options": group_options, "order_options": order_options, "current_sorting": current_sorting, "current_grouping": current_grouping, "current_ordering": current_ordering, "group_list": group_list})
 
+
 def seed_the_database(request):
     print("Hook Received")
     print(request.POST)
@@ -313,6 +324,7 @@ def seed_the_database(request):
         output = "Delivered"
 
     return JsonResponse(data={"Output": output})
+
 
 def logout_user(request):
     logout(request)
