bb8f99fa105cd74670c5806932715389f487978b
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-07-14 12:19:38 +0100

    Modified the logic for rerunning the seeder.

--- notes/custom_context_processors.py
+++ notes/custom_context_processors.py
@@ -1,11 +1,13 @@
 from .models import Folder
 import os
+from django.core.exceptions import ObjectDoesNotExist
 
 def tree_nav(request):
     try:
         root_parent = Folder.objects.get(name="All Notes")
-        nav_html = root_parent.get_folder_tree()
-    except:
+    except ObjectDoesNotExist:
         os.system("python research_notes/seed_database.py")
+        root_parent = Folder.objects.get(name="All Notes")
+    nav_html = root_parent.get_folder_tree()
     # nav_html = ""
     return {"full_nav": nav_html}
